c75779a2801422e8fde9b27a2e10460490cea54d
Pedro Unas <pedrounas@MacBook-Pro-de-Unas.local>
2020-04-29 07:26:46 +0100

    Add recommenderlab library to main, use it to make recommendations

--- main.r
+++ main.r
@@ -1,21 +1,42 @@
+require(recommenderlab)
+require(tidyverse)
+require(reshape2)
+
 data <- read.csv('Data/movie.csv')
-ratings <- read.csv('Data/Ratings.csv')
+ratings <- read.csv('Data/Ratings.csv') %>% select(userid,movieid,rating)
+sample_ratings <- ratings[sample(nrow(ratings), 100), ]
+sample_ratings <- merge(sample_ratings, data, by='movieid')
+sample_ratings <- sample_ratings %>% select(userid,moviename,rating)
+# top_critics <- head(names(sort(table(ratings$userid), decreasing = T)), 5)
+# sample_ratings <- ratings[ratings$userid %in% top_critics, ]
 
-dim(data) # 66730 movies, all with unique ID
+#### Experimental zone ###
 
-# Create average rating column by movies
-x <- table(unlist(ratings$movieid))
-y <- aggregate(cbind(rating) ~ movieid, data = ratings, sum)
-y$num_ratings <- x
-y$avg_rating <- y$rating/y$num_ratings
+x <- acast(sample_ratings, userid ~ moviename)
+R <- as.matrix(x)
+r <- as(R, "realRatingMatrix")
 
+rec_POPULAR <- Recommender(r, method='POPULAR')
+rec_COLAB_FILTER <- Recommender(r, method='UBCF')
+pred_POPULAR <- predict(rec_POPULAR, r[1:5], n = 2)
+pred_COLAB_FILTER <- predict(rec_COLAB_FILTER, r[1:5], n = 2)
+as(pred_POPULAR,"list")
+as(pred_COLAB_FILTER,"list")
 
-popular <- ifelse(y$num_ratings > median(y$num_ratings), # Consider only "popular" movies
-                  TRUE,
-                  FALSE)
-
-popular_table <- y[popular,]
-top_rated <- head(popular_table[order(popular_table$avg_rating, decreasing=T),])
-top_rated <- merge(data,top_rated[ , c("movieid", "avg_rating")], by ='movieid')
-top_rated <- top_rated[order(top_rated$avg_rating, decreasing = T),]
-top_rated
+# Old way 
+# # Create average rating column by movies
+# x <- table(unlist(ratings$movieid))
+# y <- aggregate(cbind(rating) ~ movieid, data = ratings, sum)
+# y$num_ratings <- x
+# y$avg_rating <- y$rating/y$num_ratings
+# 
+# 
+# popular <- ifelse(y$num_ratings > mean(y$num_ratings), # Consider only "popular" movies
+#                   TRUE,
+#                   FALSE)
+# 
+# popular_table <- y[popular,]
+# top_rated <- head(popular_table[order(popular_table$avg_rating, decreasing=T),])
+# top_rated <- merge(data,top_rated[ , c("movieid", "avg_rating")], by ='movieid')
+# top_rated <- top_rated[order(top_rated$avg_rating, decreasing = T),]
+# top_rated
