70e3344dbe1284c6dd685c252e9653b269b34fe4
Solange <solange.sampaio18@gmail.com>
2020-05-01 23:57:51 +0100

    association rules

--- main.r
+++ main.r
@@ -16,12 +16,33 @@ x <- acast(sample_ratings, userid ~ moviename)
 R <- as.matrix(x)
 r <- as(R, "realRatingMatrix")
 
+
 rec_POPULAR <- Recommender(r, method='POPULAR')
 rec_COLAB_FILTER <- Recommender(r, method='UBCF')
+
 pred_POPULAR <- predict(rec_POPULAR, r[1:5], n = 2)
 pred_COLAB_FILTER <- predict(rec_COLAB_FILTER, r[1:5], n = 2)
 as(pred_POPULAR,"list")
 as(pred_COLAB_FILTER,"list")
+
+
+b <- binarize(r, minRating=2.5)
+
+
+model_params <- list(support=0.001,
+                     confidence=0.8)
+
+rec_AR <- Recommender(b, "AR", parameter=model_params)
+rules <- getModel(rec_AR)$rule_base
+inspect(rules)
+
+pred_AR <- predict(rec_AR, b[1:99], n = 2)
+as(pred_AR,"list")
+
+
+
+
+
 
 # Old way 
 # # Create average rating column by movies
