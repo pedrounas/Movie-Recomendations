011dae2dc33f8aa6afcdefeacec5e84f5775d8b2
Pedro Unas <pedrounas@MacBook-Pro-de-Unas.local>
2020-05-16 01:12:48 +0100

    Update sample logic and model params

--- clean.R
+++ clean.R
@@ -13,13 +13,13 @@ ratings.date <- ratings$date # Talvez usar isto no futuro
 ratings <- ratings %>% select(userid,movieid,rating)
 
 top_critics <- ratings %>% group_by(userid) %>% filter(n() > 1000) %>% as.data.frame()
-sample_ratings <- top_critics[sample(nrow(top_critics), nrow(top_critics)/200), ]
+sample_ratings <- top_critics[sample(nrow(top_critics), nrow(top_critics)/50), ]
 sample_ratings <- merge(sample_ratings, data, by='movieid')
 sample_ratings <- sample_ratings %>% select(userid,moviename,rating)
 
 sample_ratings_binary <- sample_ratings
 sample_ratings_binary$rating <-
-  ifelse(sample_ratings_binary$rating >= 2.5,
+  ifelse(sample_ratings_binary$rating >= 3.5,
          1,
          0
   )
@@ -41,32 +41,31 @@ y <- y[!useless ,]
 r <- as(x, "realRatingMatrix")
 b <- as(y, "binaryRatingMatrix")
 model_params <- list(support=10/dim(b)[2],
-                     confidence=0.01)
+                     confidence=0.1)
 
-set.movies <-evaluationScheme(r,method='cross-validation',train=.7,given=1,goodRating=2.5,k=10)
+set.movies <-evaluationScheme(r,method='cross-validation',train=.7,given=1,goodRating=3.5,k=10)
 set.movies_ <-evaluationScheme(b,method='cross-validation',train=.7,given=1,k=10)
 
 rec_POPULAR <- Recommender(getData(set.movies,"train"), method='POPULAR')
-rec_COLAB_FILTER <- Recommender(getData(set.movies,"train"), method='UBCF')
+rec_UBCF <- Recommender(getData(set.movies,"train"), method='UBCF')
+
 rec_AR <- Recommender(getData(set.movies_,"train"), "AR", parameter=model_params)
+getModel(rec_AR)$rule_base
 
 pred_POPULAR <- predict(rec_POPULAR,getData(set.movies,"known"))
-pred_COLAB_FILTER <- predict(rec_COLAB_FILTER,getData(set.movies,"known"))
+pred_UBCF <- predict(rec_UBCF,getData(set.movies,"known"))
 pred_AR <- predict(rec_AR,getData(set.movies_,"known"))
 
 methods <- list("popular" = list(name="POPULAR", param = NULL),
                 "user-based CF" = list(name="UBCF", param = NULL))
-methods_ <- list("association rules" = list(name="AR", param = model_params))
+methods_ <- list("popular" = list(name="POPULAR", param = NULL),
+                 "user-based CF" = list(name="UBCF", param = NULL),
+                 "association rules" = list(name="AR", param = model_params))
 
 #real
 acc_REC <- evaluate(set.movies, methods, n=c(1,2,5)) 
 #binary
-b_acc_REC <- evaluate(set.movies_, methods, n=c(1,2,5)) 
-
-acc_REC_ <- evaluate(set.movies_, methods_, n=c(1,2,5))
-acc_REC$`association rules` <- acc_REC_$`association rules`
-
-# plot(acc_REC, annotate = 1:4, legend="topleft")
+b_acc_REC <- evaluate(set.movies_, methods_, n=c(1,2,5)) 
 
 avg.conf <- function(results) {
   tmp <- results %>%
@@ -93,3 +92,4 @@ ggplot(final.results.binary, aes(x=FPR, y=TPR, colour = fct_reorder2(as.factor(n
                                                                      FPR, TPR))) + geom_line() +
   geom_label(aes(label = n)) + 
   labs(title = "ROC curves", colour = "Model")
+
